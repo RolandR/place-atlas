004cdb7968026969c48e1086706ec0d498e730e9
Zach Fechko <71729723+zfechko@users.noreply.github.com>
2022-04-04 18:25:03 -0700

    added monkeytype

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":179, "name": "monkeytype", "description": "The logo of the minimalistic type testing website monkeytype, which features themes based off of numerous enthusiast keycap sets.", "website": "https://monkeytype.com", "subreddit": "/r/monkeytype", "center": [620.5, 1321.5], "path": [[589.5, 1339.5], [589.5, 1330.5], [596.5, 1330.5], [596.5, 1305.5], [599.5, 1300.5], [639.5, 1300.5], [644.5, 1304.5], [644.5, 1330.5], [651.5, 1330.5], [651.5, 1339.5]]}
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
